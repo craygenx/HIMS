d4ab7ad52f37e4024731231d75ae979351eccfde
craygenx <marshmellowking99@gmail.com>
2023-07-04 21:54:27 +0300

    added inventory board ui

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+
 const form = document.getElementById('accountForm');
+const inventoryForm = document.getElementById('ineventory');
+let userData;
 
 document.addEventListener('DOMContentLoaded', ()=>{});
 //control the registration form tabs login/signup
@@ -84,10 +87,45 @@ function alertMessage(message, color){
     }, 3000)
 }
 function loadAccountData(account, username){
+    userData = account;
     const navName = document.getElementById('navName');
     const salutations = document.getElementById('dashUsername');
     const availableItems = document.getElementById('availableCount');
     navName.textContent = `${account[username].username}`;
     salutations.textContent = `${account[username].username}`;
     availableItems.textContent = `${account[username].pantry_items.length}`;
+}
+inventoryForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const itemName = document.getElementById('itemName');
+    const itemQuantity = document.getElementById('itemQuantity');
+    const itemThreshold = document.getElementById('threshCount').value || document.getElementById('threshCount').placeholder;
+    const data = {
+        "itemName": itemName,
+        "itemQuantity": itemQuantity,
+        "itemThreshold": itemThreshold
+    }
+    handleAddItem(data)
+
+})
+function openInventoryBoard() {
+    const inventoryBoard = document.getElementById('inventoryBoard');
+    const homepage = document.getElementById('homepage');
+    inventoryBoard.style.display = 'flex';
+    homepage.style.opacity = '1';
+    homepage.style.backgroundColor = '#ffffff';
+}
+function handleAddItem(data){
+    fetch('http://localhost:3000/accounts',{
+        method: 'PATCH',
+        headers: {
+            'Content-Type':'application/json',
+            'accept':'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+}
+function closeInventory(){
+    const inventoryBoard = document.getElementById('inventoryBoard');
+    inventoryBoard.style.display = 'none';
 }
